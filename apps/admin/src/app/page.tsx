7111eeaa86531c860e32d1b133194c8ce5a90856
seob171 <seob171@github.com>
2023-10-16 23:49:51 +0900

    feat: project setup

---
+++ apps/admin/src/app/page.tsx
@@ -0,0 +1,8 @@
+const Page = () => {
+  return <div>
+    <h1>Admin</h1>
+    <button>Click Me!</button>
+  </div>
+}
+
+export default Page
---
+++ apps/store/src/app/page.tsx
@@ -0,0 +1,8 @@
+const Page = () => {
+  return <div>
+          <h1>Store</h1>
+          <button>Click Me!</button>
+          </div>
+}
+
+export default Page
